ad8b59a54dbdde253156aac4c54c122106fbd26f
Patrick Oser <patrick.oser148@gmail.com>
2022-12-03 13:30:33 -0500

    Stuck delcaring function, will only return number instead of string from given array.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Hello World!");
+function getComputerChoice() {
+    const choices = ('rock', 'paper', 'scissors');
+    const computerChoice = Math.floor(Math.random() * choices.length);
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
