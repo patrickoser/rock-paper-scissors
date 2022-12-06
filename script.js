8a50dd3e3edf8f5680a7f5dbb3fcbed41bf75e91
Patrick Oser <patrick.oser148@gmail.com>
2022-12-06 07:15:57 -0500

    Fixed the game function but that lead to other issues that still need to be resolved.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // In this case, computerChoice is a random number between 0 and 2.
 // So choices[computerChoice] will return either 'rock', 'paper', or 'scissors'.
 
+let computerScore = 0;
+let playerScore = 0;
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = Math.floor(Math.random() * choices.length);
@@ -20,44 +23,45 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++;
         return ("Paper beats rock. You lose.");
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
         return ("Scissors beats paper. You lose.");
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore++;
         return ("Rock beats scissors. You lose.");
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
         return ("Rock beats scissors. You win!");
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
         return ("Paper beats rock. You win!");
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
         return ("Scissors beats paper. You win!");
     } else {
         return ("Draw.");
     }
 }
 
-const playerSelection = ("PaPeR");
-const computerSelection = getComputerChoice();
-
 // Comments here.
 
-function game(playerScore, computerScore) {
-
-    let playerScore = 0;
-    let computerScore = 0;
+function game() {
 
     for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, paper, or scissors?")
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
 
-        playRound(playerSelection, computerSelection);
-
-        if (playerSelection == true) {
-            return playerScore++;
-        } else if (computerSelection == true) {
-            return computerScore++;
+        if (playerScore > computerScore) {
+            console.log("You win the game!")
+        } else if (playerScore < computerScore) {
+            console.log("You lose the game.")
         } else {
-            break;
+            console.log("It's a draw!")
         }
     }
 }
 
-console.log(game(playerScore, computerScore));
+console.log(game());
